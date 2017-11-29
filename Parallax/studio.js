3626955b9d73530107048cfd0cb971597b43603c
bsutherl2 <sutherland.b@husky.neu.edu>
2017-11-29 16:15:49 -0500

    nav bar

--- Parallax/studio.js
+++ Parallax/studio.js
@@ -14,3 +14,13 @@ $(window).scroll(function(){
   });
 
 });
+$(window).scroll(function () {
+    $('.scrollReveal').each(function () {
+        var imagePos = $(this).offset().top - 310;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("typewriter");
+        }
+    });
+});
