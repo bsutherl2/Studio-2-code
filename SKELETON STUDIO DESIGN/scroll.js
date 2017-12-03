4f884554e1d737aa9213703ba2d3cad1013cddd4
bsutherl2 <sutherland.b@husky.neu.edu>
2017-12-02 19:37:58 -0500

    update

---
+++ SKELETON STUDIO DESIGN/scroll.js
@@ -0,0 +1,28 @@
+$(window).scroll(function () {
+    $('.scrollReveal').each(function () {
+        var imagePos = $(this).offset().top - 310;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("typewriter");
+        }
+    });
+});
+/* Parallax js
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+$(window).scroll(function() {
+  var curentPosition = Math.round($(this).scrollTop());
+  console.log(curentPosition);
+  $(".midground").css({
+    transform: "translate(0px, -" + curentPosition / 5 + "%)"
+  });
+  $(".forground").css({
+    transform:
+      "translate(" + curentPosition / 2 + "%, " + curentPosition / 2 + "%)",
+    opacity: "0.5"
+  });
+
+  $(".text").css({
+    transform: "translate(0px, " + curentPosition / 7 + "%)"
+  });
+});
